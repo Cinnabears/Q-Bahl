2dd92fcf212ac54ad49077c86cd2e723a57e366c
Alberto Scicali <afs2842@rit.edu>
2016-03-23 17:09:05 -0400

    Player Character scripts and transformations

---
+++ Project/Assets/Scripts/Player/PlayerAiming.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAiming : MonoBehaviour {
+	public GameObject playerArm;
+
+	Vector3 mouse_pos;
+	Vector3 arm_pos;
+	private float turn_angle;
+
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		Turn ();
+	}
+
+	void Turn() {
+		mouse_pos = Input.mousePosition;
+		arm_pos = Camera.main.WorldToScreenPoint (playerArm.transform.position);
+		mouse_pos.x = mouse_pos.x - arm_pos.x;
+		mouse_pos.y = mouse_pos.y - arm_pos.y;
+		mouse_pos.z = 0f;
+		turn_angle = Mathf.Atan2 (mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg;
+		playerArm.transform.rotation = Quaternion.Euler (new Vector3 (0, 0, turn_angle));
+	}
+}
---
+++ Project/Assets/Scripts/Player/PlayerTransformManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class PlayerTransformManager : MonoBehaviour {
+
+	PlayerTransform current_form;
+	public Dictionary<int, PlayerTransform> transform_dict;
+
+	PlayerAbilities player_abilities;
+
+	// Use this for initialization
+	void Awake () {
+		player_abilities = GetComponent <PlayerAbilities> ();
+		transform_dict = new Dictionary<int, PlayerTransform> ();
+		transform_dict.Add (1, GetComponent<PlayerNormalForm> ());
+		transform_dict.Add (2, GetComponent<PlayerWarpForm> ());
+		ChangePlayerForm (1);
+	}
+
+	void Update () {
+		CheckForTransform ();
+	}
+
+	void CheckForTransform () {
+		if (Input.GetKeyDown(KeyCode.Alpha1) && current_form != transform_dict[1]) {
+			ChangePlayerForm(1);
+		}
+		if (Input.GetKeyDown(KeyCode.Alpha2) && current_form != transform_dict[2]) {
+			ChangePlayerForm (2);
+		}
+	}
+
+	void ChangePlayerForm (int form_num) {
+		current_form = transform_dict[form_num];
+		player_abilities.SetCurrentForm (current_form);
+		Debug.Log ("Transforming into " + current_form.transform_name + " form!");
+	}
+
+	public PlayerTransform GetCurrentForm () {
+		return current_form;
+	}
+}
